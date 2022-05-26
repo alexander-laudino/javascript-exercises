5f42e643ff2e7f50c56dbc5f25f87ba2c7a26d1a
Alexander Laudino <93599746+alexander-laudino@users.noreply.github.com>
2022-05-26 14:12:17 -0400

    Finish exercsie 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function (start = 0, end = 0) {
+  let sum = 0;
+  if (start > end) {
+    let temp = start;
+    start = end;
+    end = temp;
+  }
+  if (
+    start < 0 ||
+    end < 0 ||
+    !(typeof start === "number") ||
+    !(typeof end === "number")
+  ) {
+    sum = "ERROR";
+  } else {
+    for (start; start <= end; start++) sum += start;
+  }
+  return sum;
 };
 
 // Do not edit below this line
