335f6cff78bb85f692451059b5ebdb0cbd5e1926
Alexander Laudino <93599746+alexander-laudino@users.noreply.github.com>
2022-05-26 15:23:12 -0400

    Finish exercise 06_leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,9 @@
-const leapYears = function() {
-
+const leapYears = function (year = 0) {
+  let isLeap = false;
+  if (year % 4 === 0 && year % 100 === 0) {
+    if (year % 400 === 0) isLeap = true;
+  } else if (year % 4 === 0) isLeap = true;
+  return isLeap;
 };
 
 // Do not edit below this line
