e87dba2239c419eeb76d710aabe17f8fce393cf4
Alexander Laudino <93599746+alexander-laudino@users.noreply.github.com>
2022-05-26 15:58:18 -0400

    Finish exercise 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,17 @@
-const ftoc = function() {
-
+const ftoc = function (temp = 0) {
+  let celsius = (temp - 32) * (5 / 9);
+  if (!(celsius % 1 === 0)) celsius = parseFloat(celsius.toFixed(1));
+  return celsius;
 };
 
-const ctof = function() {
-
+const ctof = function (temp = 0) {
+  let fahr = temp * (90 / 50) + 32;
+  if (!(fahr % 1 === 0)) fahr = parseFloat(fahr.toFixed(1));
+  return fahr;
 };
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
