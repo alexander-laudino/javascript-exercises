073f53d6441b5dc3ad6876f1ba66f2282937db57
Alexander Laudino <93599746+alexander-laudino@users.noreply.github.com>
2022-05-26 13:43:13 -0400

    Finish exercise 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array = [], ...args) {
+  let argsArray = args;
+  for (const item of argsArray) {
+    if (!array.includes(item)) {
+      continue;
+    }
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] === item) {
+        array.splice(i, 1);
+      }
+    }
+  }
+  return array;
 };
 
 // Do not edit below this line
