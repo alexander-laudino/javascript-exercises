3398c44846fa1293fd52721999fd52425ec8f7a3
Alexander Laudino <93599746+alexander-laudino@users.noreply.github.com>
2022-06-01 08:34:39 -0400

    Add findTheOldest function, pass test, finish exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+  let sortedPeeps = people.sort((a, b) => {
+    let aFutureDate, bFutureDate;
+    if (a.yearOfDeath === undefined) {
+      aFutureDate = new Date().getFullYear();
+    } else {
+      aFutureDate = a.yearOfDeath;
+    }
+    const lastPerson = aFutureDate - a.yearOfBirth;
+    if (b.yearOfDeath === undefined) {
+      bFutureDate = new Date().getFullYear();
+    } else {
+      bFutureDate = b.yearOfDeath;
+    }
+    const nextPerson = bFutureDate - b.yearOfBirth;
+    return lastPerson > nextPerson ? -1 : 1;
+  });
+  return sortedPeeps[0];
 };
 
 // Do not edit below this line
